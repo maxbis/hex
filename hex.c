4a9918d4b03bc887a566603c44be98d099dd7d9e
maxbis <max@ffkijken.com>
2016-01-24 11:24:15 +0100

    1st upload

---
+++ hex.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+    char ch;
+    FILE *fp;
+
+    if ( argc == 1 ) {
+        fp = stdin;
+    } 
+
+    if ( argc == 2 ) {
+        if ((fp = fopen( argv[1], "rb")) == NULL ) {
+            perror("Error opening file");
+            return EXIT_FAILURE;
+        }
+    } 
+
+   int i=0;
+   char line[17];
+   
+   line[16]='\0';
+
+   printf("%09d: ",i);
+   while( ( ch = fgetc(fp) ) != EOF ) {
+          printf("%02X ",(unsigned char)ch);
+          if ( ch>=32 && ch<=126 ) {
+             line[i%16] = ch;
+          } else {
+             line[i%16] = '.';
+          }
+          if( !(++i % 16) ) {
+              printf("  %s\n%09d: ",line, i);
+          } else {	
+              if ( !(i%8) ) printf(" ");
+          }
+   }
+   line[i%16] = '\0';
+
+   while ( i%16 ) {     
+       if ( !(i%8) ) printf(" ");
+       printf("   ");
+       i++;
+   }
+   printf("  %s\n",line);
+   fclose(fp);
+   putc('\n', stdout);
+
+   return 0;
+}
